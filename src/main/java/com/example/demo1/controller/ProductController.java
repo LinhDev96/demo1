36655b3baa17ba341239d41eae034898064e3fce
LinhDev96 <linhpro45@gmail.com>
2022-05-06 07:53:49 +0700

    Initial commit

---
+++ src/main/java/com/example/demo1/controller/ProductController.java
@@ -0,0 +1,65 @@
+package com.example.demo1.controller;
+
+import com.example.demo1.Models.Product;
+import com.example.demo1.Models.ResponseObject;
+import com.example.demo1.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "/api/v1/Products")
+public class ProductController {
+    @Autowired
+    private ProductRepository repository;
+
+    @GetMapping("")
+    List<Product> getAllProducts(){
+    return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    ResponseEntity<ResponseObject> findById(@PathVariable Long id){
+        Optional<Product> foundProduct = repository.findById(id);
+        if(foundProduct.isPresent()){
+            return ResponseEntity.status(HttpStatus.OK).body(
+                    new ResponseObject("ok","Query product successfully ",foundProduct)
+            );
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                    new ResponseObject("false","Can not find product with id = ",""));
+        }
+    }
+    @PostMapping("/insert")
+    ResponseEntity<ResponseObject> insertProduct(@RequestBody Product newProduct){
+        List<Product> foundProduct = repository.findByProductName(newProduct.getProductName().trim());
+        if(foundProduct.size()>0){
+            return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).body(
+                    new ResponseObject("false","Product already exist","")
+            );
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new ResponseObject("ok","Insert successfully",repository.save(newProduct))
+        );
+    }
+    @PutMapping("/{id}")
+    ResponseEntity<ResponseObject> updateProduct(@RequestBody Product newProduct, @PathVariable Long id) {
+        Product updatedProduct = repository.findById(id)
+                .map(product -> {//trung nen luu de
+                    product.setProductName(newProduct.getProductName());
+                    product.setYear(newProduct.getYear());
+                    product.setPrice(newProduct.getPrice());
+                    return repository.save(product);// truyen ham product vao day
+                }).orElseGet(() -> {// insert moi
+                    newProduct.setId(id);
+                    return repository.save(newProduct);
+                });
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new ResponseObject("ok", "Update Product successfully", updatedProduct)
+        );
+    }
+}
---
+++ src/main/java/com/example/demo1/database/Database.java
@@ -0,0 +1,26 @@
+package com.example.demo1.database;
+
+import com.example.demo1.Models.Product;
+import com.example.demo1.repositories.ProductRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Database {
+    private static final Logger logger = LoggerFactory.getLogger(Database.class);
+    @Bean
+    CommandLineRunner initDatabase(ProductRepository productRepository){
+        return new CommandLineRunner() {
+            @Override
+            public void run(String... args) throws Exception {
+                Product productA = new Product("fsf",2020,2500,"");
+                Product productB = new Product("fseff",2021,250,"");
+                logger.info("insert data: " + productRepository.save(productA));
+                logger.info("insert data: " + productRepository.save(productB));
+            }
+        };
+    }
+}
